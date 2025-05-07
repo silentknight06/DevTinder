7e8c9f24ef4ec90900e82a8b2eb5a6f0c2f38ce6
Deepak Kumar <deepakroy7393@gmail.com>
2025-05-07 18:18:38 +0530

    dynamic data in signup Api

--- src/app.js
+++ src/app.js
@@ -1,18 +1,36 @@
 const express = require("express");
+ const connectDb = require("./config/database");
 const app = express();
-app.get("/user",(req, resp)=>{
-     resp.send("this server created by deepak kumar")
-});
-app.post("/user", (req, resp)=>{
-    resp.send("hey i am hello...")
-});
-app.delete("/user", (req, resp)=>{
-    resp.send("hey this is /deepak servet")
-});
-app.use("/user", (req, resp)=>{
-    resp.send("hey this is /deepak servet")
-});
+const User = require("./models/user")
 
-app.listen(3000,()=>{  
-    console.log("server is runing...")
-});
+app.use(express.json());
+app.post("/signup", async(req, resp)=>{
+    // console.log(req.body);
+    const user = new User(req.body);
+    try{
+        await user.save();
+        resp.send("user added");
+    }
+    catch (err) {
+        resp.status(400).send("error saving  the code " + err.message);
+    }
+
+})
+
+
+
+
+connectDb()
+.then(()=>{
+    console.log("🎉finally Db is connected")
+    app.listen(3000, () => {
+        console.log("Server running on http://localhost:3000");
+      });
+})
+.catch(()=>{
+    console.log("Db  can't be connected")
+})
+
+
+
+
