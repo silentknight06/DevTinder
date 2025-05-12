907d6d80e7589f003e3cec0e14be8b9ec1429f2d
Deepak Kumar <deepakroy7393@gmail.com>
2025-05-12 12:30:24 +0530

    Validate Schema

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,7 @@
 // 
-
+const validator = require("validator");
 const mongoose  = require("mongoose");
 const userSchema = new mongoose.Schema({
-   
   firstName:{
     type : String,
     require:true,
@@ -18,10 +17,20 @@ const userSchema = new mongoose.Schema({
     unique: true, // all email should unique
     lowercase: true, //  take as a lowercase value
     trim:true, // remove the extra space
+    validate(value){
+      if(!validator.isEmail(value)){
+        throw new Error("invalid Email....give corect Email" + value);
+      }
+    }
   },
   password:{
       type: String,
       require: true,
+       validate(value){
+      if(!validator.isStrongPassword(value)){
+        throw new Error("your password is weak....give Strong password" + value);
+      }
+    }
   },
   age:{
       type:Number,
@@ -38,7 +47,12 @@ const userSchema = new mongoose.Schema({
   },
   photoUrl:{
     type:String,
-    default:"https://ctmirror.org/wp-content/uploads/2021/01/dummy-man-570x570-1.png"
+    default:"https://ctmirror.org/wp-content/uploads/2021/01/dummy-man-570x570-1.png",
+     validate(value){
+      if(!validator.isURL(value)){
+        throw new Error("invalid URl....give corect URL" + value);
+      }
+    }
   },
   about:{
     type:String,
