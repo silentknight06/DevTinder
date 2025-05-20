07a9f42a1fc1c5d3cc1f60109cedf1dcd00bf9bd
Deepak Kumar <deepakroy7393@gmail.com>
2025-05-20 18:06:46 +0530

    Debug SignUp Api

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,63 +1,71 @@
 const express = require("express");
 const authRouter = express.Router();
-const {validateSignUpData} = require("../utils/validation");
-const User  = require("../models/user.js")
+const { validateSignUpData } = require("../utils/validation");
+const User = require("../models/user.js");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-  authRouter.post("/signup", async(req, resp)=>{
-    try{
-    validateSignUpData(req);   // validation of data
-     // encryption of password
+authRouter.post("/signup", async (req, resp) => {
+  try {
+    validateSignUpData(req); // validation of data
+    // encryption of password
 
-    const {firstName, lastName, emailId , password} = req.body;
+    const { firstName, lastName, emailId, password } = req.body;
     const passwordHash = await bcrypt.hash(password, 10);
-    console.log(passwordHash)
+    console.log(passwordHash);
 
     // console.log(req.body);
-    
-        const user = new User({
-            firstName, lastName, emailId, password:passwordHash
-        })
-          await user.save();
-          resp.send("added succesfully")
+
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
+    const saveUser = await user.save();
+
+    const token = await saveUser.getJWT();
+
+    resp.cookie("token", token, {
+      expires: new Date(Date.now() + 10 * 24 * 60 * 60 * 1000),
+    });
+
+    resp.json({ message: "added succesfully", data: saveUser });
+  } catch (err) {
+    resp.status(400).send("ERROR: " + err.message);
+  }
+});
+
+authRouter.post("/login", async (req, resp) => {
+  try {
+    const { emailId, password } = req.body;
+    const user = await User.findOne({ emailId: emailId });
+    if (!user) {
+      throw new Error("User is not valid");
     }
-    catch (err){
-        resp.status(400).send("ERROR: " + err.message);
+    const isPasswordValid = await user.validatePassword(password);
+    if (isPasswordValid) {
+      //create a jwt token
+      const token = await user.getJWT();
+
+      resp.cookie("token", token, {
+        expires: new Date(Date.now() + 10 * 24 * 60 * 60 * 1000),
+      });
+
+      resp.send(user);
+    } else {
+      throw new Error("User is not correct");
     }
+  } catch (err) {
+    resp.status(400).send("Err : " + err.message);
+  }
+});
+
+authRouter.post("/logout", async (req, resp) => {
+  resp.cookie("token", null, {
+    expires: new Date(Date.now()),
   });
-
- authRouter.post("/login", async(req, resp)=>{
-    try{
-         const {emailId, password} = req.body;
-         const user = await User.findOne({emailId: emailId});
-         if(!user){
-            throw new Error("User is not valid");
-         }
-         const isPasswordValid = await user.validatePassword(password);
-         if(isPasswordValid){
-            //create a jwt token
-            const token = await user.getJWT();
-
-           resp.cookie("token", token,{
-            expires: new Date(Date.now() + 10 * 24 * 60 * 60 * 1000),
-           });
-
-            resp.send(user);
-         }else{
-              throw new Error("User is not correct");
-         }
-        }
-    catch (err){
-        resp.status(400).send("Err : "+ err.message);
-    }
-  } )
-
-authRouter.post("/logout", async(req, resp)=>{
-   resp.cookie("token", null, {
-      expires:new Date(Date.now())});
-   resp.send("log out succesfully");
-})
-
+  resp.send("log out succesfully");
+});
 
 module.exports = authRouter;
--- src/routes/user.js
+++ src/routes/user.js
@@ -5,7 +5,7 @@ const {userAuth} = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
 const USER_SAFE_DATA  = ["firstName", "lastName", "photoUrl","age","gender", "about", "skills" ];
 
-userRouter.get("/user/request/received", userAuth, async (req, resp)=>{
+userRouter.get("/user/requests/received", userAuth, async (req, resp)=>{
     try{
        const loggedInUser = req.user;
        const connectionRequest = await ConnectionRequest.find({
