9f793867e5b548ea383a4f3b9a0eae4d19a18814
Deepak Kumar <deepakroy7393@gmail.com>
2025-05-21 21:10:34 +0530

    DB Connected to Atlas

--- src/app.js
+++ src/app.js
@@ -8,10 +8,11 @@ const cookieParser = require("cookie-parser");
 const cors = require('cors');
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials:true,
-}
-));
+  origin: (origin, callback) => {
+    callback(null, origin); // reflect the request origin
+  },
+  credentials: true,
+}));
 
 const authRouter = require("./routes/auth.js");
 const profileRouter = require("./routes/profile.js");
@@ -25,12 +26,12 @@ app.use("/", userRouter);
 
  connectDB()
  .then(()=>{
-    console.log("dekho db connect ho gaya 😁");
+    console.log("Db Connected Sussesfully 😁");
     app.listen(3000, ()=>{
         console.log("app is listeingn is 300000")
     })
  })
     
  .catch(()=>{
-    console.log("error loading.....")
+    console.log(" DB Can't connected....  error!")
  })
--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 
 const mongoose = require("mongoose");
 const connectDb = async() =>{
-   await mongoose.connect(process.env.DB_CONNECTION_SECRET);
+"await mongoose.connect(mongodb+srv://tinder:MuqaOunszrSGrVHi@dev.jjgti1i.mongodb.net/?retryWrites=true&w=majority&appName=dev";
 }
 module.exports = connectDb;
