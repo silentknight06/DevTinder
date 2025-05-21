a302ecdb23caf9d9df934b0b29c7fa5b7504d361
Deepak Kumar <deepakroy7393@gmail.com>
2025-05-21 17:37:04 +0530

    Remove Console.log

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,5 @@
 
 const mongoose = require("mongoose");
-console.log(process.env.DB_CONNECTION_SECRET);
-console.log(process.env.JWT_SECRET);
 const connectDb = async() =>{
    await mongoose.connect(process.env.DB_CONNECTION_SECRET);
 }
