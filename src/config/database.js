e91e27ede4f28e4933b993456062ddc821d20800
Deepak Kumar <deepakroy7393@gmail.com>
2025-05-21 17:35:48 +0530

    Added dotenv file

--- src/config/database.js
+++ src/config/database.js
@@ -1,28 +1,8 @@
-// {
-// const mongoose = require("mongoose");
-// const connectDb = async()=>{
-//     await mongoose.connect("mongodb://127.0.0.1:27017/localDb");
-        
-// }
-// module.exports = connectDb;
-
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect("mongodb://127.0.0.1:27017/localDb");
-//     console.log("MongoDB connected locally");
-//   } catch (error) {
-//     console.error("MongoDB connection failed:", error.message);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
-// }
 
 const mongoose = require("mongoose");
-
+console.log(process.env.DB_CONNECTION_SECRET);
+console.log(process.env.JWT_SECRET);
 const connectDb = async() =>{
-   await mongoose.connect("mongodb://127.0.0.1:27017/devTinder");
+   await mongoose.connect(process.env.DB_CONNECTION_SECRET);
 }
 module.exports = connectDb;
